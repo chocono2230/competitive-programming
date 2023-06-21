490b4fec9650ee4a94f81c2695551199bc9244de
chocono2230 <61158776+chocono2230@users.noreply.github.com>
2023-06-21 20:59:20 +0900

    exec処理を修正

--- AtCoder/Problems/ahc020/ah_230620_1/script/exec.sh
+++ AtCoder/Problems/ahc020/ah_230620_1/script/exec.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-if [ $# != 1 ]; then
+if [ $# -lt 1 ]; then
     echo 引数エラー: $#
+    echo \<test_case_number\> \<code\> \<code\> ...
     exit 1
 fi
 
@@ -10,10 +11,10 @@ f=./sh_input/$(printf "%04d" $1).txt
 head -n 1 $f
 cat $f > ./in.txt
 
-z=`pwd`/past/v2.cpp
-g++ -std=gnu++17 -Wall -Wextra -O2 -DCHOMONOLIS_LOCAL -DCHOCONO_LOCAL $z
-cat ./in.txt | ./a.out > output.txt
 
-z=`pwd`/ahc020_ah_230620_1.cpp
-g++ -std=gnu++17 -Wall -Wextra -O2 -DCHOMONOLIS_LOCAL -DCHOCONO_LOCAL $z
-cat ./in.txt | ./a.out > output.txt
+for ((i=2; i <= $#; i++)); do
+    z=`pwd`/${!i}
+    echo ${!i}
+    g++ -std=gnu++17 -Wall -Wextra -O2 -DCHOMONOLIS_LOCAL -DCHOCONO_LOCAL $z
+    cat ./in.txt | ./a.out > output.txt
+done
